8b3fea5271b75953550793ac34a1c1232e4eaf36
apple <apple@agapes-Pro.local>
2025-09-28 20:24:13 +0800

    feat: implement user authentication with JWT

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+#abstract user
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
-
+#abstract user
 # Create your models here.
